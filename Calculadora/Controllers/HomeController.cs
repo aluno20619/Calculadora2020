a40fe6743ce56dc64c1f937bf04e8f3bb27881b7
claudia <aluno20619@ipt.pt>
2020-03-19 12:30:37 +0000

    Inicio da programacao do controler

--- Calculadora/Controllers/HomeController.cs
+++ Calculadora/Controllers/HomeController.cs
@@ -20,11 +20,58 @@ namespace Calculadora.Controllers
 
         public IActionResult Index()
         {
+            //inicializar valor
+            ViewBag.Visor = "0";
             return View();
         }
         [HttpPost]
-        public IActionResult Index(string visor,string bt)
+        public IActionResult Index(string visor,string bt,string primeiroValor,string operador)
         {
+            //filtrar o conteudo dobt
+            switch (bt)
+            {
+                case "1":
+                case "2":
+                case "3":
+                case "4":
+                case "5":
+                case "6":
+                case "7":
+                case "8":
+                case "9":
+                case "0":
+                    //processar algarismos
+                    if (visor == "0") visor = bt;
+                    else visor = visor + bt;
+                break;
+                case "+\-":
+                    visor = Convert.ToDouble(visor)* -1 + "";
+                break;
+                case ",":
+                    if (!visor.Contains(",")) visor += ",";
+                break;
+                case "+":
+                case "-":
+                case ":":
+                case "*":
+                    //processar os operadores
+                    //ativar  a 'memoria' do http
+                    if (operador == null)
+                    {
+                        ViewBag.PrimeiroValor = visor;
+                        ViewBag.Operador = bt;
+                    }
+                    else
+                    {
+                        ;
+                    }
+                    //falta limpar o ecra
+                    break;
+
+            }//swich
+            //exportar dados para a view
+            ViewBag.Visor = visor;
+           
             return View();
         }
 
