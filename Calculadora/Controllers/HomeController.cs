1cdbcac038d64eab954abfb9a8a8b73b5511cdd4
claudia <aluno20619@ipt.pt>
2020-04-01 16:20:57 +0100

    conclusao da calcuadora

--- Calculadora/Controllers/HomeController.cs
+++ Calculadora/Controllers/HomeController.cs
@@ -25,7 +25,7 @@ namespace Calculadora.Controllers
             return View();
         }
         [HttpPost]
-        public IActionResult Index(string visor,string bt,string primeiroValor,string operador)
+        public IActionResult Index(string limpaVisor,string visor,string bt,string primeiroValor,string operador)
         {
             //filtrar o conteudo dobt
             switch (bt)
@@ -41,34 +41,83 @@ namespace Calculadora.Controllers
                 case "9":
                 case "0":
                     //processar algarismos
-                    if (visor == "0") visor = bt;
+                    if (visor == "0"||limpaVisor=="true") visor = bt;
                     else visor = visor + bt;
-                break;
-                case "+\-":
+
+                    ViewBag.PrimeiroOperador = primeiroValor;
+                    ViewBag.Operador = operador;
+                    break;
+                case "+/-" :
                     visor = Convert.ToDouble(visor)* -1 + "";
-                break;
+                    ViewBag.PrimeiroOperador = primeiroValor;
+                    ViewBag.Operador = operador;
+                    break;
                 case ",":
                     if (!visor.Contains(",")) visor += ",";
-                break;
+                    ViewBag.PrimeiroOperador = primeiroValor;
+                    ViewBag.Operador = operador;
+                    ViewBag.LimpaVisor = "false";
+                    break;
                 case "+":
                 case "-":
                 case ":":
                 case "*":
+                case "=":
                     //processar os operadores
                     //ativar  a 'memoria' do http
+                    // -123,8
                     if (operador == null)
                     {
                         ViewBag.PrimeiroValor = visor;
                         ViewBag.Operador = bt;
+                        //daV ordem de limpar o visor
+                        ViewBag.LimpaVisor = "true";
                     }
+                    
                     else
                     {
-                        ;
-                    }
-                    //falta limpar o ecra
-                    break;
+                        //ja carregou pelo menos uma 2nda vez no sinal de operador
+                        double auxprimeiroOperando = Convert.ToDouble(primeiroValor);
+                        double auxsegundoOperando = Convert.ToDouble(visor);
+                        //executar a operacao
+                        switch(operador){
+                            case "+" :
+                                visor = auxprimeiroOperando + auxsegundoOperando+"";
+                            break;
+                            case "-":
+                                visor = auxprimeiroOperando - auxsegundoOperando + "";
+                                break;
+                            case "*":
+                                visor = auxprimeiroOperando * auxsegundoOperando + "";
+                                break;
+                            case ":":
+                                visor = auxprimeiroOperando / auxsegundoOperando + "";
+                                break;
+                           
 
-            }//swich
+                        }
+                        //ativar servico de memoria do http
+                        ViewBag.PrimeiroOperador = visor;
+                        ViewBag.Operador = bt;
+                        //limpar
+                        ViewBag.LimpaVisor = "true";
+                    }
+                    if (bt == "=")
+                    {
+                        //anular o efeito dos operadores
+                        ViewBag.Operador = null;
+
+                    }
+                    break;
+                case "C" :
+                    ViewBag.PrimeiroOperador = null;
+                    ViewBag.Operador = null;
+                    //limpar
+                    ViewBag.LimpaVisor = "true";
+                    visor = "0";
+                    break;
+            
+        }//swich
             //exportar dados para a view
             ViewBag.Visor = visor;
            
